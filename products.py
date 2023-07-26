451213e99154424c8abddd738ff671a7e3137300
Wei Siong <p36115014@gs.ncku.edu.tw>
2023-07-26 11:09:31 +0800

    add csv

--- products.py
+++ products.py
@@ -12,3 +12,8 @@ print(products) #印出打清單
 
 for p in products: #用for loop一條一條地印出大清單中的資料（小清單）
     print(p)
+
+#寫入檔案
+with open("products.csv","w") as f: #有沒有檔案都沒關係，沒有的話會自動新增 #副檔名txt>csv
+    for p in products:
+        f.write(p[0] + "," + p[1] + "\n") #用"," 比較適合excel
