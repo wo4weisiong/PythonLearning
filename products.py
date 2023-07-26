bf1a757a05b7f74ed508fd54d839ba9525e0ecab
Wei Siong <p36115014@gs.ncku.edu.tw>
2023-07-26 11:28:44 +0800

    encoding utf-8

--- products.py
+++ products.py
@@ -4,7 +4,7 @@ while True:
     product = input("請輸入商品名稱：(q for quit)")
     if product == "q":
         break
-    price = input("請輸入價格：")
+    price = int(input("請輸入價格："))
     p = [product, price] #建立小清單
     products.append(p) #將小清單列入打清單
 
@@ -14,6 +14,7 @@ for p in products: #用for loop一條一條地印出大清單中的資料（小�
     print(p)
 
 #寫入檔案
-with open("products.csv","w") as f: #有沒有檔案都沒關係，沒有的話會自動新增 #副檔名txt>csv
+with open("products.csv", "w", encoding="utf-8") as f: #有沒有檔案都沒關係，沒有的話會自動新增 #副檔名txt>csv #加入encoding編碼
+    f.write("商品,價格\n") #在數據前先寫入欄位名稱
     for p in products:
-        f.write(p[0] + "," + p[1] + "\n") #用"," 比較適合excel
+        f.write(p[0] + "," + str(p[1]) + "\n") #用"," 比較適合excel #str()將int轉換成字串
